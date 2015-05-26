a1a909c5bc21f2816a603d0dd88ee506b11a7201
Markus Thulin <macky_b@hotmail.com>
2015-05-26 11:30:24 +0200

    Prepare for PHPUnit

---
+++ test/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Config
+*
+* Get all configuration details to be able to execute the test suite.
+*
+* PHP version 5
+*
+* @category Config
+* @package  Arsenal_Webscrape
+* @author   Markus Thulin <macky_b@hotmail.com>
+* @license  http://www.opensource.org/licenses/mit-license.php MIT
+* @link     https://github.com/thulin82/arsenal_webscrape
+*/
+require __DIR__ . "/../autoloader.php";
+?>
